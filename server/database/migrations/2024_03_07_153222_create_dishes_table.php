76dffce6ea02a57ae706c69c89f292c6e22fd78f
oleggud512 <oleggud52@gmail.com>
2024-03-07 17:47:49 +0200

    create Dish model

--- server/database/migrations/2024_03_07_153222_create_dishes_table.php
+++ server/database/migrations/2024_03_07_153222_create_dishes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('category');
             $table->timestamps();
         });
     }
