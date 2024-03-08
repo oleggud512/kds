dab9281017a1a626028e47baf7208e86a6bc5512
oleggud512 <oleggud52@gmail.com>
2024-03-08 13:45:30 +0200

    mobile: add dio

--- mobile_client/lib/src/core/di/get_it.config.dart
+++ mobile_client/lib/src/core/di/get_it.config.dart
@@ -8,8 +8,10 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:mobile_client/src/core/di/get_it.dart' as _i5;
 import 'package:mobile_client/src/router.dart' as _i3;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -23,7 +25,11 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
+    final registerModule = _$RegisterModule();
     gh.lazySingleton<_i3.AppRouter>(() => _i3.AppRouter());
+    gh.lazySingleton<_i4.Dio>(() => registerModule.dio);
     return this;
   }
 }
+
+class _$RegisterModule extends _i5.RegisterModule {}
--- mobile_client/lib/src/core/di/get_it.dart
+++ mobile_client/lib/src/core/di/get_it.dart
@@ -1,7 +1,19 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:dio/dio.dart';
+import 'package:mobile_client/src/core/common/extensions/string.dart';
 import 'get_it.config.dart';
 
+
+@module
+abstract class RegisterModule {
+
+  @lazySingleton
+  Dio get dio => Dio(BaseOptions(
+    baseUrl: "http://localhost:8000/".hardcoded
+  ));
+  
+}
 
 @InjectableInit()
 GetIt configureDependencies() => GetIt.instance.init();
