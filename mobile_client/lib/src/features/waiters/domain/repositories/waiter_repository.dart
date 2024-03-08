34ac94d8a83e69e0ceaa1e86ce67061dd57ec152
oleggud512 <oleggud52@gmail.com>
2024-03-08 07:54:12 +0200

    WaiterRepository interface

---
+++ mobile_client/lib/src/features/waiters/domain/repositories/waiter_repository.dart
@@ -0,0 +1,7 @@
+import 'package:either_dart/either.dart';
+import 'package:mobile_client/src/core/domain/exceptions.dart';
+import 'package:mobile_client/src/features/waiters/domain/entities/waiter.dart';
+
+abstract interface class WaiterRepository {
+  Either<AppException, List<Waiter>> getWaiters();
+}
