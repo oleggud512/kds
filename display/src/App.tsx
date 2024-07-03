85aaad57ce15ec4e1c45ca954fbb3348777a1232
oleggud512 <oleggud52@gmail.com>
2024-07-03 08:03:17 +0300

    clean display

--- display/src/App.tsx
+++ display/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './App.css'
 import Layout from './pages/Layout';
-import Main from './pages/Main';
 import Kitchen from './pages/Kitchen';
-import Admin from './pages/Admin';
 import NoPage from './pages/NoPage';
 
-function KitchenOnlyRouter() {
+function MyRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -17,23 +15,10 @@ function KitchenOnlyRouter() {
   )
 }
 
-function FullRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />} />
-        <Route path="kitchen" element={<Kitchen />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
-  )
-}
-
 function App() {
   return (
     <BrowserRouter>
-      <KitchenOnlyRouter />
+      <MyRouter />
     </BrowserRouter>
   )
 }
